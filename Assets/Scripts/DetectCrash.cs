ffb32249c0e94fd1885ce2f505d0977f4b07d834
Caleb Turner <turner632@morrisville.edu>
2022-02-22 18:52:33 -0500

    NameSpaces & SceneManagement

--- Assets/Scripts/DetectCrash.cs
+++ Assets/Scripts/DetectCrash.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class DetectCrash : MonoBehaviour
 {   void OnTriggerEnter2D(Collider2D other) 
    {
        if(other.tag == "ground")
        {
-           Debug.Log("You hit your head!");
+           SceneManager.LoadScene(0);
        }
    }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-void OnTriggerEnter2D(Collider2D other)
-{
-    if(other.tag == "Player")
+    void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("You finished");
+        if(other.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+        }
     }
-
-   
-}
 
 }
