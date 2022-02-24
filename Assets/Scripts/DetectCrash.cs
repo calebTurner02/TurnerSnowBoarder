f7119ebacfd941068dc19b2e4d79167061ffc9b4
Caleb Turner <turner632@morrisville.edu>
2022-02-24 15:27:46 -0500

    Using Invoke() for Delays

--- Assets/Scripts/DetectCrash.cs
+++ Assets/Scripts/DetectCrash.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class DetectCrash : MonoBehaviour
-{   void OnTriggerEnter2D(Collider2D other) 
+
+{  
+    [SerializeField] float fltloadDelay = 0.5f;
+     void OnTriggerEnter2D(Collider2D other) 
    {
        if(other.tag == "ground")
        {
+           Invoke("ReloadScene", fltloadDelay);
+       }
+     
+   }
+     void ReloadScene()
+       {
            SceneManager.LoadScene(0);
        }
-   }
-}
+    }
+
+
+
